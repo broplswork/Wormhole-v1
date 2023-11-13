0d36c49a27dc841aafacb0e06201e7d66e80cfe9
broplswork <143388875+broplswork@users.noreply.github.com>
2023-11-13 03:34:13 +0000

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ const proxyMiddleware = createProxyMiddleware({
 // Use the proxy middleware
 app.use('/proxy', proxyMiddleware);
 
+// Serve static files from the public directory
+app.use(express.static('public'));
+
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// Serve static files (e.g., CSS, JS)
-app.use(express.static('public'));
 
 // Set up the route for handling search requests
 app.get('/search', (req, res) => {
