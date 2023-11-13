c79c8c8b5da73d0290e509c928df42265d40fc3f
broplswork <143388875+broplswork@users.noreply.github.com>
2023-11-13 05:17:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
-const url = require('url'); // Import the 'url' module for URL validation
 
 const app = express();
 
-// Create a proxy middleware
 const proxyMiddleware = createProxyMiddleware({
-  changeOrigin: true, // Needed for virtual hosted sites
+  changeOrigin: true,
 });
 
-// Use the proxy middleware for all routes under /search
 app.use('/search', (req, res) => {
   const targetUrl = req.query.url;
+  const searchEngine = req.query.engine || 'google';
 
-  // Validate the user-input URL
   if (!isValidUrl(targetUrl)) {
     res.status(400).send('Invalid URL');
     return;
   }
 
-  // Proxy the request to the specified URL
+  let target = '';
+  if (searchEngine === 'bing') {
+    target = 'https://www.bing.com/search?q=';
+  } else {
+    target = 'https://www.google.com/search?q=';
+  }
+
+  req.url = target + encodeURIComponent(targetUrl);
   proxyMiddleware(req, res, (err) => {
     if (err) {
       console.error(err);
@@ -29,31 +33,25 @@ app.use('/search', (req, res) => {
   });
 });
 
-// Serve static files from the public directory
 app.use(express.static('public'));
 
-// Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Set up the default route
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-// Function to validate a URL
 function isValidUrl(userInput) {
   try {
     const parsedUrl = new URL(userInput);
-    // Add more validation if needed
     return true;
   } catch (error) {
     return false;
   }
 }
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
