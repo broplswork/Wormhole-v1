d5030e638dc600f0acff2a5137421afc530291cf
broplswork <143388875+broplswork@users.noreply.github.com>
2023-12-26 23:55:53 +0000

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,1 +1,39 @@
+function visit() {
+    const inputUrl = document.getElementById('urlInput').value;
+    const proxyUrl = 'https://proxc.minoa.cat/?link=' + encodeURIComponent(btoa(inputUrl));
+    const proxyFrame = document.getElementById('proxyFrame');
+    proxyFrame.src = proxyUrl;
+}
+
+fetch('apps.json')
+    .then(response => response.json())
+    .then(data => {
+        const apps = data;
+
+        function displayApps() {
+            const appsContainer = document.querySelector('.apps-section');
+
+            apps.forEach(app => {
+                const appLink = document.createElement('a');
+                appLink.href = '#';
+                appLink.textContent = app.name;
+                appsContainer.appendChild(appLink);
+
+                const appLogo = document.createElement('img');
+                appLogo.src = app.logo;
+                appLogo.alt = `${app.name} Logo`;
+                appLink.appendChild(appLogo);
+
+                appLink.addEventListener('click', function(event) {
+                    event.preventDefault();
+                    const proxyUrl = 'https://proxc.minoa.cat/?link=' + encodeURIComponent(btoa(app.url));
+                    const proxyFrame = document.getElementById('proxyFrame');
+                    proxyFrame.src = proxyUrl;
+                });
+            });
+        }
+
+        displayApps();
+    })
+    .catch(error => console.error('Error fetching apps data:', error));
 
