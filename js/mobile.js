df31a407d7f4f6d85a0deca10d4b4781f987345d
_Eckermania <127853724+Eckerman-ia@users.noreply.github.com>
2024-01-29 12:16:30 -0500

    Delete js directory

--- js/mobile.js
+++
@@ -1,9 +0,0 @@
-let details = navigator.userAgent;
-let regexp = /android|iphone|kindle|ipad/i;
-let isMobileDevice = regexp.test(details);
-if (isMobileDevice) {
-    alert("Doge Unblocker has detected that you are accessing the website on mobile. Please use a computer instead as this website was not designed for mobile devices.");
- window.location.href = 'mobile.html';
-} else {
-    console.log('I love big tall guys twerking in the shower');
-}
